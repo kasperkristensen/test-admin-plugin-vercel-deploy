7294a1ab6bcc34c147ef5ec9a317e1372c259222
Kasper <kasper@medusajs.com>
2023-03-08 16:04:17 +0100

    fix tailwind configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-const path = require("path")
-
 module.exports = {
-  plugins: [
-    require("tailwindcss/nesting"),
-    require("tailwindcss")({
-      config: path.join(__dirname, "tailwind.config.js"),
-    }),
-    require("autoprefixer"),
-  ],
+    plugins: {
+        "tailwindcss/nesting": {},
+        tailwindcss: {},
+        autoprefixer: {},
+    }
 }
